672bdc040d966dd70176cbef73dfc5542f1f7c45
jun kobayashi <junkobayashi1025@gmail.com>
2021-03-02 09:32:44 +0000

    post_showリンク付け

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,11 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+
+  end
+
   private
   def post_params
     params.require(:post).permit(:caption, photos_attributes:[:image]).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
   resources :users
   resources :sessions
-  resources :posts, only: [:new, :create, :index] do
+  resources :posts, only: [:new, :create, :index, :show] do
     resources :photos, only: [:create]
   end
 end
