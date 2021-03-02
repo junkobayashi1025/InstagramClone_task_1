d3cc7eefb77c965e69fa00cbc1bdfa0697109d26
jun kobayashi <junkobayashi1025@gmail.com>
2021-03-02 06:15:13 +0000

    users&posts&photosのアソシエーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
+  has_many :posts, dependent: :destroy
 end
