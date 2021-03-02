6dcf902942875e50667e9700f382b4b44651c3e2
jun kobayashi <junkobayashi1025@gmail.com>
2021-03-02 11:36:20 +0000

    Likeモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
   has_many :posts, dependent: :destroy
+  has_many :likes
 end
